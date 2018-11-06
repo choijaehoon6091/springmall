dcf27f5d48ad9ae374c9c607162193cf6ee6ce82
smart-202-10 <smart-202-10@smart-202-10-PC>
2018-11-06 10:11:53 +0900

    [최재훈]

--- springmall/src/main/java/com/example/springmall/HiController.java
+++ springmall/src/main/java/com/example/springmall/HiController.java
@@ -1,12 +1,13 @@
 package com.example.springmall;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HiController {
 	@RequestMapping("/hi")
-	public void hi() {
+	public String hi() {
 		System.out.println("Hi Spring Boot!");
+		return "hi"; //forward -> WEB-INF/jsp/hi.jsp
 	}
 }
