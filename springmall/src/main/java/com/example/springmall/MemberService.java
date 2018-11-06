109f72e369287839f3d6ef4b1eaded17793eb8f9
smart-202-10 <smart-202-10@smart-202-10-PC>
2018-11-06 12:22:44 +0900

    [최재훈] springmall
    1. memberservice 트랜잭셔널

--- springmall/src/main/java/com/example/springmall/MemberService.java
+++ springmall/src/main/java/com/example/springmall/MemberService.java
@@ -2,8 +2,11 @@ package com.example.springmall;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
+
 public class MemberService {
 	@Autowired
 	private MemberMapper memberMapper;
