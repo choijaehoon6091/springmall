26e732c33dd395e2bc4db155129bc02461a13d77
smart-202-10 <smart-202-10@smart-202-10-PC>
2018-11-06 09:18:31 +0900

    [최재훈] springmall 시작
    1.pom.xml
    2.application properties 수정

---
+++ springmall/src/main/java/com/example/springmall/HiController.java
@@ -0,0 +1,12 @@
+package com.example.springmall;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+	@RequestMapping("/hi")
+	public void hi() {
+		System.out.println("Hi Spring Boot!");
+	}
+}
