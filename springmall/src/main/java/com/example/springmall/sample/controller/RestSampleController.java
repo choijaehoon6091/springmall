0de3df748e2d4911c741f77a3e4aaf5571deb930
smart-202-10 <smart-202-10@smart-202-10-PC>
2018-11-09 10:04:01 +0900

    [최재훈] sample 세션 추가 중..
    1. LoginController 추가
    2. RestSampleController 추가

--- springmall/src/main/java/com/example/springmall/sample/controller/RestSampleController.java
+++ springmall/src/main/java/com/example/springmall/sample/controller/RestSampleController.java
@@ -1,5 +1,14 @@
 package com.example.springmall.sample.controller;
 
-public class RestSampleController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springmall.sample.vo.Sample;
+
+@RestController
+public class RestSampleController { // REST API
+	@RequestMapping(value="/sample/getRestSample")
+	public Sample getRestSample() {
+		return new Sample(1,"id001","pw001"); //{"sampleNo:1"
+	}
 }
